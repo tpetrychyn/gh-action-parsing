0eee6794004515aff5782dbd8c26288b2260184a
Taylor Petrychyn <tpetrych@Taylors-MacBook-Pro.local>
2022-03-31 20:47:09 -0700

    get comment body

--- dist/index.js
+++ dist/index.js
@@ -8475,17 +8475,15 @@ async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
     const octokit = github.getOctokit(GITHUB_TOKEN)
 
+    const COMMENT_BODY = core.getInput('COMMENT_BODY')
+
     const { context = {} } = github
     const { pull_request } = context.payload
-
-    console.log('pr number' + pull_request.number)
-    console.log(context)
-    console.log(octokit)
 
     await octokit.rest.issues.createComment({
         ...context.repo,
         issue_number: pull_request.number,
-        body: 'Thanks'
+        body: 'Thanks you entered ' + COMMENT_BODY
     })
 }
 
