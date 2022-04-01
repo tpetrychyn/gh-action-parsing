27fd9112a6880cc4356f5adf2d2d64f7f85ff988
Taylor Petrychyn <tpetrych@Taylors-MacBook-Pro.local>
2022-03-31 20:33:11 -0700

    add comment on new pr

--- src/action.js
+++ src/action.js
@@ -1,5 +1,18 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
 async function run() {
-    console.log('Hello world')
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+    const octokit = github.getOctokit(GITHUB_TOKEN)
+
+    const { context = {} } = github
+    const { pull_request } = context.payload
+
+    await octokit.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request.issue_number,
+        body: 'Thanks'
+    })
 }
 
 run()
